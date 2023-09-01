1256e1da83aee0f41e57d3efffd171bb30df3495
happylife0322 <best.dev0322@gmail.com>
2023-09-01 21:07:14 +0800

    Add remove recode feature

--- src/pages/WpPage/index.tsx
+++ src/pages/WpPage/index.tsx
@@ -1,10 +1,11 @@
-import { ReactNode, useEffect, useState } from 'react';
+import { Key, useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import './wpPage.scss'
 import Layout from '../../components/Template/Layout';
 import api from '../../services/Api';
 import { Badge, Button, Card, Col, Row, Table, TableColumnsType, Typography, message, Input } from 'antd';
 import type { TableRowSelection } from 'antd/es/table/interface';
+import { MinusCircleFilled } from '@ant-design/icons';
 
 const { Text } = Typography;
 
@@ -19,8 +20,12 @@ interface DataType {
 
 function WpPage() {
   const { cwa_id } = useParams();
-  const [dataTable, setDataTable] = useState();
+  const [dataTable, setDataTable] = useState<any>([]);
   const [fetchingData, setFetchingData] = useState(false);
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [subdescription, setSubDescription] = useState('');
+  const [selectedKey, setSelectedKey] = useState<Key>();
   const navigate = useNavigate();
 
 
@@ -29,11 +34,13 @@ function WpPage() {
     api.get("/v1/cwas/" + cwa_id + "/wps")
       .then((response) => {
         if (response.status === 200) {
-          console.log(response.data.data)
-          const data = response.data.data;
-          const table = data.map((obj: any) => ({
-            ...obj,
-            key: obj.id
+          const table = response.data.data.map((obj: any) => ({
+            key: obj.id,
+            name: obj.name,
+            description: obj.discipline_name,
+            subdisciplina: obj.sub_discipline_name,
+            area: 'Geral',
+            state: 'Finalizado'
           }));
 
           setDataTable(table);
@@ -45,7 +52,12 @@ function WpPage() {
   }, []);
 
   const columns: TableColumnsType<DataType> = [
-    { title: 'Nome', dataIndex: 'name', key: 'name', render: (name, record) => (<a style={{color: 'black'}} onClick={() => navigate("/ewp/" + record.key)}>{name}</a>) },
+    {
+      title: 'Nome', dataIndex: 'name', key: 'name', render: (name, record) => {
+        if (record.key == selectedKey) return <><span style={{ color: 'black' }}>{name}</span> <a onClick={() => removeRecord(record.key)}><MinusCircleFilled style={{ color: 'red', marginLeft: 20 }} /></a></>;
+        return <a style={{ color: 'black' }} onClick={() => navigate("/ewp/" + record.key)}>{name}</a>
+      }
+    },
     { title: 'Área', dataIndex: 'area', key: 'area' },
     { title: 'Disciplina', dataIndex: 'description', key: 'description' },
     { title: 'Subdisciplina', dataIndex: 'subdisciplina', key: 'subdisciplina' },
@@ -53,16 +65,20 @@ function WpPage() {
   ];
 
   const rowSelection: TableRowSelection<DataType> = {
-    onChange: (selectedRowKeys, selectedRows) => {
-      console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
-    },
-    onSelect: (record, selected, selectedRows) => {
-      console.log(record, selected, selectedRows);
-    },
-    onSelectAll: (selected, selectedRows, changeRows) => {
-      console.log(selected, selectedRows, changeRows);
+    onSelect: (record) => {
+      setSelectedKey(record.key);
     },
   };
+
+  const removeRecord = (key: Key) => {
+    const newData = dataTable.filter((item: any) => item.key !== key);
+    setDataTable(newData);
+  }
+
+  const insertHandle = () => {
+    if (name == '') return false;
+    setDataTable([...dataTable, { name: name, description: description, subdisciplina: subdescription, state: 'Finalizado', area: 'Geral' }]);
+  }
 
   return (
     <>
@@ -78,7 +94,10 @@ function WpPage() {
               <Table
                 className='table-cwa'
                 columns={columns}
-                rowSelection={rowSelection}
+                rowSelection={{
+                  type: 'radio',
+                  ...rowSelection,
+                }}
                 dataSource={dataTable}
                 loading={fetchingData}
                 pagination={false}
@@ -90,10 +109,10 @@ function WpPage() {
             </Col>
           </Row>
           <Row justify={'center'} className='table-insert'>
-            <Col span={6}><Input placeholder="Nome" /></Col>
-            <Col span={6}><Input placeholder="Disciplina" /></Col>
-            <Col span={6}><Input placeholder="Subdisciplina" /></Col>
-            <Col span={4} sm={3} lg={2}><Button type="primary">Inserir</Button></Col>
+            <Col span={6}><Input placeholder="Nome" value={name} onChange={e => setName(e.target.value)} /></Col>
+            <Col span={6}><Input placeholder="Disciplina" value={description} onChange={e => setDescription(e.target.value)} /></Col>
+            <Col span={6}><Input placeholder="Subdisciplina" value={subdescription} onChange={e => setSubDescription(e.target.value)} /></Col>
+            <Col span={4} sm={3} lg={2}><Button type="primary" onClick={insertHandle}>Inserir</Button></Col>
           </Row>
         </Card>
       </Layout>
