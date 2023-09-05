ec22e66f8f380373835de14030472ca13417a0b0
happylife0322 <best.dev0322@gmail.com>
2023-09-05 20:46:50 +0800

    Fix EWP page

--- src/pages/EwpPage/index.tsx
+++ src/pages/EwpPage/index.tsx
@@ -1,32 +1,34 @@
-import { Badge, Breadcrumb,  Card, Col,  Progress,  RadioChangeEvent,  Row, Table, Tabs, Typography } from 'antd';
+import { Badge, Breadcrumb, Card, Col, Progress, RadioChangeEvent, Row, Table, Tabs, Typography } from 'antd';
 
 import './ewpPage.scss';
 import Layout from '../../components/Template/Layout';
-import {  useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { SizeType } from 'antd/es/config-provider/SizeContext';
+import { useParams } from 'react-router-dom';
+import api from '../../services/Api';
 
 const { Text } = Typography;
 
 function EwpPage() {
-
+  const { wp_id } = useParams();
   const [size, setSize] = useState<SizeType>('small');
-  
+  const [items, setItems] = useState<any>([]);
 
-
+  const groupByKey = (list: any, key: any) => list.reduce((hash: any, obj: any) => ({ ...hash, [obj[key]]: (hash[obj[key]] || []).concat(obj) }), {})
 
   const dataSource = [
     {
       key: '1',
       atividades: 'Certificar desenhos de fornecedor e liberar para construção - CH-1290KN-01',
       responsavel: 'Contratada T',
-      progresso:  <Progress percent={33} status="active" />
-      
+      progresso: <Progress percent={33} status="active" />
+
     },
     {
       key: '2',
       atividades: 'Elaborar documentos para fornecimento de equipamentos mecânicos - CH-1290KN-01',
       responsavel: 'Contratada J',
-      progresso:  <Progress percent={98} status="active" />
+      progresso: <Progress percent={98} status="active" />
     },
   ];
 
@@ -35,19 +37,19 @@ function EwpPage() {
       key: '1',
       atividades: 'Elaborar desenhos de fabricação e montagem de equipamentos mecânicos - CH-1290KN-01',
       responsavel: 'Contratada X',
-      progresso:  <Progress percent={28} status="active" />
+      progresso: <Progress percent={28} status="active" />
     },
     {
       key: '2',
       atividades: 'Fornecer equipamentos mecânicos - CH-1290KN-01',
       responsavel: 'Contratada Z',
-      progresso:  <Progress percent={75} status="active" />
+      progresso: <Progress percent={75} status="active" />
     },
     {
       key: '3',
       atividades: 'Transportar e entregar equipamentos mecânicos no site - CH-1290KN-01',
       responsavel: 'Contratada Z',
-      progresso:  <Progress percent={97} status="active" />
+      progresso: <Progress percent={97} status="active" />
     }
   ];
 
@@ -56,27 +58,27 @@ function EwpPage() {
       key: '1',
       atividades: 'Realizar a prontidão do CWP de equipamentos mecânicos - CH-1290KN-01',
       responsavel: 'Contratada T',
-      progresso:  <Progress percent={33} status="active" />
-      
+      progresso: <Progress percent={33} status="active" />
+
     },
     {
       key: '2',
       atividades: 'Montar equipamentos mecânicos - CH-1290KN-01',
       responsavel: 'Contratada J',
-      progresso:  <Progress percent={50} status="active" />
+      progresso: <Progress percent={50} status="active" />
     },
     {
       key: '3',
       atividades: 'Realizar verificação da qualidade mecânica - CH-1290KN-01',
       responsavel: 'Contratada T',
-      progresso:  <Progress percent={43} status="active" />
-      
+      progresso: <Progress percent={43} status="active" />
+
     },
     {
       key: '4',
       atividades: 'Formalizar a completação mecânica do TWP - CH-1290KN-01',
       responsavel: 'Contratada J',
-      progresso:  <Progress percent={98} status="active" />
+      progresso: <Progress percent={98} status="active" />
     },
   ];
 
@@ -85,17 +87,17 @@ function EwpPage() {
       key: '1',
       descricao: 'Lorem Ipsum - CH-1290KN-01',
       responsavel: 'Contratada T',
-      progresso:  <Progress percent={33} status="active" />
-      
+      progresso: <Progress percent={33} status="active" />
+
     },
     {
       key: '2',
       descricao: 'Lorem Ipsum - CH-1290KN-01',
       responsavel: 'Contratada J',
-      progresso:  <Progress percent={98} status="active" />
+      progresso: <Progress percent={98} status="active" />
     },
   ];
-  
+
   const columns = [
     {
       title: 'Atividades',
@@ -135,34 +137,69 @@ function EwpPage() {
 
 
 
-  const items = [
-    {
-      label: <Badge status="success" text="EWP - Engenharia" />,
-      key: 'ewp',
-      children: <Table dataSource={dataSource} columns={columns} />,
-    },
-    {
-      label: <Badge status="success" text="PWP - Fornecimento" />,
-      key: 'pwp',
-      children: <Table dataSource={dataSource2} columns={columns} />,
-    },
-    {
-      label: <Badge status="warning" text="CWP - Construção" />,
-      key: 'cwp',
-      children: <Table dataSource={dataSource3} columns={columns}  />,
-    },
-  ];
 
   const items2 = [
     {
       label: <Badge status="success" text="Passos" />,
       key: 'passos',
       children: <Table dataSource={dataSource4} columns={columns2} />,
-    }    
+    }
   ];
 
+  const fetchItems = async () => {
+    await api.get(`v1/wps/${wp_id}/activities`)
+      .then((response) => {
+        if (response.status === 200) {
+          const allData = groupByKey(response.data.data, 'type');
+          const types = Object.keys(groupByKey(response.data.data, 'type'));
+          var items = [];
+          if (types.includes('EWP')) {
+            const source = allData['EWP'].map((obj: any) => ({
+              key: obj.id,
+              atividades: obj.name,
+              responsavel: obj.sub_discipline_name,
+              progresso: <Progress percent={100} status="active" />
+            }));
+            items.push({
+              label: <Badge status="success" text="EWP - Engenharia" />,
+              key: 'ewp',
+              children: <Table dataSource={source} columns={columns} />,
+            })
+          }
+          if (types.includes('PWP')) {
+            const source = allData['PWP'].map((obj: any) => ({
+              key: obj.id,
+              atividades: obj.name,
+              responsavel: obj.sub_discipline_name,
+              progresso: <Progress percent={100} status="active" />
+            }));
+            items.push({
+              label: <Badge status="success" text="PWP - Fornecimento" />,
+              key: 'pwp',
+              children: <Table dataSource={source} columns={columns} />,
+            })
+          }
+          if (types.includes('CWP')) {
+            const source = allData['CWP'].map((obj: any) => ({
+              key: obj.id,
+              atividades: obj.name,
+              responsavel: obj.sub_discipline_name,
+              progresso: <Progress percent={100} status="active" />
+            }));
+            items.push({
+              label: <Badge status="warning" text="CWP - Construção" />,
+              key: 'cwp',
+              children: <Table dataSource={source} columns={columns} />,
+            })
+          }
+          setItems(items);
+        }
+      });
+  }
 
-
+  useEffect(() => {
+    fetchItems();
+  }, [])
 
   return (
     <>
@@ -175,20 +212,20 @@ function EwpPage() {
         <Card size="small" title="CWA - Áreas do Projeto" extra={''}>
           <Row>
             <Col span={24}>
-              
-            <Breadcrumb
-              items={[
-                {
-                  title: 'Home',
-                },
-                {
-                  title: <a href="/a">KN-N1344-290 - N2 - Mina - Geral</a>,
-                },
-                {
-                  title: <a href="/b">KN-N1344-290-M-EM-0001</a>,
-                }                
-              ]}
-            />
+
+              <Breadcrumb
+                items={[
+                  {
+                    title: 'Home',
+                  },
+                  {
+                    title: <a href="/a">KN-N1344-290 - N2 - Mina - Geral</a>,
+                  },
+                  {
+                    title: <a href="/b">KN-N1344-290-M-EM-0001</a>,
+                  }
+                ]}
+              />
 
               <Tabs
                 defaultActiveKey="1"
